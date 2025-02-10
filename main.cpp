73b484a0e0f8ee8d750366ad6a3e3198d3cdb3c7
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-02-10 22:05:34 +0200

    mediana

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "main.h"
 
+bool compare(const Studentas &a, const Studentas &b) {
+    return a.galutinis < b.galutinis;
+}
+
 int main(){
     vector<Studentas> studentai;
     Studentas st;
-    int m, n;
-    cout<<"Kiek bus studentu?"; cin>>m; 
-    cout<<"Kiek bus namu darbu vienam studentui?"; cin>>n;
-    
+    int m, n, pasirinkimas;
+    cout<<"Kiek bus studentu? "; cin>>m; 
+    cout<<"Kiek bus namu darbu vienam studentui? "; cin>>n;
+    cout<<"Spauskite 1, jeigu norite, kad galutiniam balui butu naudojamas vidurkis, 2 - jeigu mediana: "; 
+    cin>>pasirinkimas;
+    while(pasirinkimas!=1 && pasirinkimas!=2)
+    {
+        cout<<"Prasau spauskite 1, jeigu norite, kad galutiniam balui butu naudojamas vidurkis, 2 - jeigu mediana: "; 
+        cin>>pasirinkimas;
+    }
     for(int i=0; i<m; i++)
     {
     cout<<"Iveskite studento varda: "; cin>>st.vardas;
@@ -27,8 +37,23 @@ int main(){
     st.galutinis=0.4*vidurkis+0.6*st.egz;
     studentai.push_back(st);
     st.nd.clear();
+    if(pasirinkimas==2)
+    {
+        sort(studentai.begin(), studentai.end(), compare);
+        for(int i=0; i<m; i++)
+        {
+            if(n%2==0)
+                st.galutinis=0.4*st.nd[n/2-1]+0.6*st.egz;
+            else
+                st.galutinis=0.4*st.nd[n/2]+0.6*st.egz;
+        }
+        cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Med.)"<<"\n"<<string(50, '-')<<endl;
+        cout<<left<<setw(15)<<st.pavarde<<setw(15)<<st.vardas<<setw(15)<<fixed<<setprecision(2)<<st.galutinis<<endl;
+    }
+    else{
     cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Vid.)"<<"\n"<<string(50, '-')<<endl;
     cout<<left<<setw(15)<<st.pavarde<<setw(15)<<st.vardas<<setw(15)<<fixed<<setprecision(2)<<st.galutinis<<endl;
+    }
 }
     return 0;
 }
--- main.h
+++ main.h
@@ -2,10 +2,12 @@
 #include <string>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
+
 
 using std::cout; using std::cin; using std::endl; using std::string; using std::vector;
 using std::fixed; using std::setprecision; using std::setw; using std::left;
-
+using std::sort;
 struct Studentas {
     string vardas="", pavarde="";
     int egz=0;
