ff5fe1c0e06057843d15698d1cacc9cdb3eca9a5
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-02-11 00:33:18 +0200

    mediana_pataisymas1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
 #include "main.h"
 
-bool compare(const Studentas &a, const Studentas &b) {
-    return a.galutinis < b.galutinis;
-}
-
 int main(){
     vector<Studentas> studentai;
     Studentas st;
@@ -12,18 +8,27 @@ int main(){
     cout<<"Kiek bus namu darbu vienam studentui? "; cin>>n;
     cout<<"Spauskite 1, jeigu norite, kad galutiniam balui butu naudojamas vidurkis, 2 - jeigu mediana: "; 
     cin>>pasirinkimas;
-    while(pasirinkimas!=1 && pasirinkimas!=2)
+    
+    while(pasirinkimas!=1 && pasirinkimas!=2 )
     {
         cout<<"Prasau spauskite 1, jeigu norite, kad galutiniam balui butu naudojamas vidurkis, 2 - jeigu mediana: "; 
+        if (!(cin >> pasirinkimas)) {
+            cout << "Negalima rasyti raidziu!\n";
+            cin.clear(); // clear the error state
+            // discard everything remaining in the buffer up to newline
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            //continue;
+        }
+        else cout<<"Neteisingas pasirinkimas. ";
         cin>>pasirinkimas;
     }
     for(int i=0; i<m; i++)
     {
-    cout<<"Iveskite studento varda: "; cin>>st.vardas;
-    cout<<"Iveskite studento pavarde: "; cin>>st.pavarde;
-    cout<<"Iveskite namu darbu rezultatus: ";
+    cout<<"Iveskite "<<i+1<<"-ojo studento varda: "; cin>>st.vardas;
+    cout<<"Iveskite "<<i+1<<"-ojo studento pavarde: "; cin>>st.pavarde;
+    cout<<"Iveskite "<<i+1<<"-ojo studento namu darbu rezultatus: ";
     for(int j=0; j<n; j++){
-        cout<<j+1<<"-ojo namu darbo rezultatas: ";
+        cout<<j+1<<"-asis namu darbu rezultatas: ";
         int laik;
         cin>>laik;
         st.nd.push_back(laik);
@@ -35,25 +40,36 @@ int main(){
         vidurkis+=st.nd[i];
     vidurkis/=n;
     st.galutinis=0.4*vidurkis+0.6*st.egz;
-    studentai.push_back(st);
-    st.nd.clear();
     if(pasirinkimas==2)
     {
-        sort(studentai.begin(), studentai.end(), compare);
+        //Mediana:
+        sort(st.nd.begin(), st.nd.end());
         for(int i=0; i<m; i++)
         {
             if(n%2==0)
-                st.galutinis=0.4*st.nd[n/2-1]+0.6*st.egz;
+                st.galutinis=0.4*(st.nd[n/2-1]+st.nd[n/2])/2+0.6*st.egz;
             else
                 st.galutinis=0.4*st.nd[n/2]+0.6*st.egz;
         }
-        cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Med.)"<<"\n"<<string(50, '-')<<endl;
-        cout<<left<<setw(15)<<st.pavarde<<setw(15)<<st.vardas<<setw(15)<<fixed<<setprecision(2)<<st.galutinis<<endl;
     }
-    else{
-    cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Vid.)"<<"\n"<<string(50, '-')<<endl;
-    cout<<left<<setw(15)<<st.pavarde<<setw(15)<<st.vardas<<setw(15)<<fixed<<setprecision(2)<<st.galutinis<<endl;
-    }
+    studentai.push_back(st);
+    st.nd.clear();
 }
+    if(pasirinkimas==1) //Vidurkis
+    {
+        cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Vid.)"<<"\n"<<string(50, '-')<<endl;
+        for(int i=0; i<m; i++)
+        {
+            cout<<left<<setw(15)<<studentai[i].pavarde<<setw(15)<<studentai[i].vardas<<setw(15)<<fixed<<setprecision(2)<<studentai[i].galutinis<<endl;
+        }
+    }
+    else if(pasirinkimas==2) //Mediana
+    {
+        cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (Med.)"<<"\n"<<string(50, '-')<<endl;
+        for(int i=0; i<m; i++)
+        {
+            cout<<left<<setw(15)<<studentai[i].pavarde<<setw(15)<<studentai[i].vardas<<setw(15)<<fixed<<setprecision(2)<<studentai[i].galutinis<<endl;
+        }
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -3,11 +3,11 @@
 #include <iomanip>
 #include <vector>
 #include <algorithm>
-
+#include <limits>
 
 using std::cout; using std::cin; using std::endl; using std::string; using std::vector;
 using std::fixed; using std::setprecision; using std::setw; using std::left;
-using std::sort;
+using std::sort; using std::numeric_limits; using std::streamsize;
 struct Studentas {
     string vardas="", pavarde="";
     int egz=0;
