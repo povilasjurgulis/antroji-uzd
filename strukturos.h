aa2e2ce2330ac2444e9146ae370c2b18ffe36966
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-03-03 16:37:38 +0200

    sukurta laiko skaičiavimo klasė "timer"

--- strukturos.h
+++ strukturos.h
@@ -11,5 +11,18 @@ struct Studentas {
 struct Pasirinkimas{
     int m=0, n=0, pasirinkimas=0, a=3, b=5, c=0, choice=0, e=0, choice1=0, file_kiekis=0;
 };
-
+class Timer {
+    private:
+      using hrClock = std::chrono::high_resolution_clock;
+      using durationDouble = std::chrono::duration<double>;
+      std::chrono::time_point<hrClock> start;
+    public:
+      Timer() : start{ hrClock::now() } {}
+      void reset() {
+        start = hrClock::now();
+      }
+      double elapsed() const {
+        return durationDouble (hrClock::now() - start).count();
+      }
+  };
 #endif
