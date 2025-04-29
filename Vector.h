96897aa55b8e17b7303a754e8cfa90d0dc341296
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-04-29 22:43:59 +0300

    Pridėjau daugiau metodų, kaip at(), front(), size() ir taip toliau

--- Vector.h
+++ Vector.h
@@ -3,30 +3,41 @@
 
 template <typename T>
 class Vector{
-    private:
-        int size;
+    
+        size_t size;
+        size_t capacity;
         T* data;
     public:
-        Vector(int size = 0); // Konstruktorius
+
+        Vector(); // Konstruktorius
+        Vector(size_t size);  // Konstruktorius su dydžiu
         Vector(const Vector& other); // Copy konstruktorius
         Vector(Vector&& other) noexcept; // Move konstruktorius
         ~Vector(); // Destruktorius
         Vector& operator=(const Vector& other); // Copy priskyrimo operatorius
         Vector& operator=(Vector&& other) noexcept; // Move priskyrimo operatorius
-        int& operator[](int index); // Operatorius [] (priskyrimas)
-        const int& operator[](int index) const; // Operatorius [] (skaitymas)
-        int GetSize() const; // Grazina vektoriaus dydi
-        void SetSize(int size); // Nustato vektoriaus dydi
+        T& operator[](size_t index);        // prieiga be tikrinimo
+        const T& operator[](size_t index) const;
 
-        void PushBack(const T& value); // Prideda elementa i vektoriu
-        void PopBack(); // Istrina paskutini elementa is vektoriaus
-        void Clear(); // Istrina visus elementus is vektoriaus
-        void Resize(int newSize); // Pakeicia vektoriaus dydi
-        void ShrinkToFit(); // Pakeicia vektoriaus dydi pagal realiai uzimama atminties dydi
-        void Reserve(int newCapacity); // Rezervuoja atminties vieta vektoriui
-        void Swap(Vector& other); // Pakeicia vektoriu vietomis
-        void Sort(); // Surikiuoja vektoriu
-        void Reverse(); // Apvercia vektoriu
+        T& at(size_t index);                // prieiga su tikrinimu (gal throw)
+        const T& at(size_t index) const;
+        T& front();                         // pirmas elementas
+        T& back();                          // paskutinis elementas
+        
+        // Dydžio ir talpos funkcijos:
+        size_t size() const;                // grąžina dydį
+        size_t capacity() const;            // grąžina talpą
+        bool empty() const;                 // ar vektorius tuščias
+        void reserve(size_t new_capacity);  // padidina talpą (bet nekeičia dydžio)
+        void resize(size_t new_size);       // keičia dydį (gali sukurti naujus elementus)
+
+        void push_back(const T& value); // Prideda elementa i vektoriu
+        void pop_back(); // Istrina paskutini elementa is vektoriaus
+        void clear(); // Istrina visus elementus is vektoriaus
+        void shrink_to_fit(); // Pakeicia vektoriaus dydi pagal realiai uzimama atminties dydi
+        void swap(Vector& other); // Pakeicia vektoriu vietomis
+        void sort(); // Surikiuoja vektoriu
+        void reverse(); // Apvercia vektoriu
 };
 
 #endif
